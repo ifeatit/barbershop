2ebcb8449f4edd2d5b28a3f5d26c431a659b89e6
Yury Zaytsev <more.jorand@gmail.com>
2015-10-13 00:43:55 +0200

    added styles and js for ie8

---
+++ js/ie8.js
@@ -0,0 +1,6 @@
+//Clear margins for nth-child, last-child
+$(".appointment-form input:nth-child(2n)").addClass("nth-child-2n");
+$(".main-contacts .btn:last-child").addClass("btn-last-child");
+$(".gallery").addClass("gallery-live");
+$(".gallery-togles").addClass("gallery-togles-visible");
+$(".catalog-item:nth-child(3n)").addClass("nth-child-3n");
